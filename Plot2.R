bf7479d879706a29f93d19173e1693a88b9b0aca
Mark Gray <mgray@dingo.com>
2016-10-23 00:28:35 +1000

    Add files via upload

--- Plot2.R
+++ Plot2.R
@@ -2,7 +2,7 @@
 SummaryPM25 <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 #Total Emission for Baltimore City, Maryland Per Year for 1999, 2002, 2005, and 2008
-subSetMaryland <- Summary[SummaryPM25$fips== "24510",]
+subSetMaryland <- SummaryPM25[SummaryPM25$fips== "24510",]
 yearlyTotals <- aggregate(Emissions ~ year, subSetMaryland, sum)
 #Create PNG file
 png(file="Plot2.png", width = 640, height = 480, pointsize = 12, bg = "white")
