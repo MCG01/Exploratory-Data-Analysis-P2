72dd4dd318806d3b32b5fa286f5c46344120e605
Mark Gray <mgray@dingo.com>
2016-10-24 12:04:19 +1000

    Add files via upload

--- Plot1.R
+++ Plot1.R
@@ -1,11 +1,18 @@
+
 ## PLOT 1 - Mark Gray
+
+# Load files into dataframes
 SummaryPM25 <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
-#Total Emission Per Year for 1999, 2002, 2005, and 2008
+
+# Total Emission Per Year for 1999, 2002, 2005, and 2008
 yearlyTotals <- aggregate(Emissions ~ year, SummaryPM25, sum)
-#Create PNG file
+
+# Create PNG file
 png(file="Plot1.png", width = 640, height = 480, pointsize = 12, bg = "white")
-#Create Plot
-barplot(height=yearlyTotals$Emissions, names.arg=yearlyTotals$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
-#Close
+
+# Create Plot
+barplot(height=yearlyTotals$Emissions, names.arg=yearlyTotals$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions from 1999 to 2008'))
+
+# Close
 dev.off()
